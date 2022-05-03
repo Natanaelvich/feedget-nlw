48f6f237d03ca6b2199edb544c0936831d9a7467
Natanael Lima <taelima1997@gmail.com>
2022-05-03 00:20:56 -0300

    add structure componnet

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import { ChatTeardropDots } from "phosphor-react";
+import React from "react";
 
+const Widget: React.FC = () => {
+  return (
+    <div className="absolute bottom-4 right-4">
+      <button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+        <ChatTeardropDots className="w-6 h-6" />
 
-const Widget : React.FC = () => {
-    return (
-       <h1></h1>
-    );
+        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+          <span className="pl-2"></span>
+          Feedback
+        </span>
+      </button>
+    </div>
+  );
 };
 
 export default Widget;
