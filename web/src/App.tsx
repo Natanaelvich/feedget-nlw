56c3b1dc3c6fdfe361454ed66133df42080481cb
Natanael Lima <taelima1997@gmail.com>
2022-05-02 23:59:43 -0300

    update with widget component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,7 @@
+import Widget from "./components/Widget";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <Widget />;
 }
 
 export default App;
