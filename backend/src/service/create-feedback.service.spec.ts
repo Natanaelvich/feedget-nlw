ec1baeacfe44d32939012e97a491c64c0ab18ad8
Natanael Lima <taelima1997@gmail.com>
2022-05-05 21:41:19 -0300

    update tests CreateFeedbackService

--- backend/src/service/create-feedback.service.spec.ts
+++ backend/src/service/create-feedback.service.spec.ts
@@ -1,22 +1,20 @@
-import { CreateFeedbackService } from './create-feedback.service';
+import { CreateFeedbackService } from "./create-feedback.service";
 
-describe('CreateFeedbackService', () => {
-  let createFeedback: CreateFeedbackService;
-  const createFeedbackSpy = jest.fn();
-  const sendMailSpy = jest.fn();
+const createFeedbackSpy = jest.fn();
+const sendMailSpy = jest.fn();
 
-  beforeEach(async () => {
-    createFeedback = new CreateFeedbackService(
-      { create: createFeedbackSpy },
-      { sendMail: sendMailSpy }
-    );
-  });
-  it('should be able to submit a feedback', async () => {
+const createFeedback = new CreateFeedbackService(
+  { create: createFeedbackSpy },
+  { sendMail: sendMailSpy }
+);
+
+describe("CreateFeedbackService", () => {
+  it("should be able to submit a feedback", async () => {
     await expect(
       createFeedback.execute({
-        type: 'bug',
-        comment: 'I have a bug',
-        screenshot: 'data:image/png;base64,dasdsadasd3e32eeewdasdas'
+        type: "bug",
+        comment: "I have a bug",
+        screenshot: "data:image/png;base64,dasdsadasd3e32eeewdasdas",
       })
     ).resolves.not.toThrow();
 
@@ -24,32 +22,32 @@ describe('CreateFeedbackService', () => {
     expect(sendMailSpy).toHaveBeenCalled();
   });
 
-  it('should not be able to submit feedback without type', async () => {
+  it("should not be able to submit feedback without type", async () => {
     await expect(
       createFeedback.execute({
-        type: '',
-        comment: 'I have a bug',
-        screenshot: 'data:image/png;base64,dasdsadasd3e32eeewdasdas'
+        type: "",
+        comment: "I have a bug",
+        screenshot: "data:image/png;base64,dasdsadasd3e32eeewdasdas",
       })
     ).rejects.toThrow();
   });
 
-  it('should not be able to submit feedback without comment', async () => {
+  it("should not be able to submit feedback without comment", async () => {
     await expect(
       createFeedback.execute({
-        type: 'bug',
-        comment: '',
-        screenshot: 'data:image/png;base64,dasdsadasd3e32eeewdasdas'
+        type: "bug",
+        comment: "",
+        screenshot: "data:image/png;base64,dasdsadasd3e32eeewdasdas",
       })
     ).rejects.toThrow();
   });
 
-  it('should not be able to submit feedback with an invalid screenshot', async () => {
+  it("should not be able to submit feedback with an invalid screenshot", async () => {
     await expect(
       createFeedback.execute({
-        type: 'bug',
-        comment: 'I have a bug',
-        screenshot: '123'
+        type: "bug",
+        comment: "I have a bug",
+        screenshot: "123",
       })
     ).rejects.toThrow();
   });
