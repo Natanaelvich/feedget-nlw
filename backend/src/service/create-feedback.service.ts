7bc2e082a0572ae531d513831e516dd795e8e6b6
Natanael Lima <taelima1997@gmail.com>
2022-05-05 21:19:27 -0300

    update CreateFeedbackService

--- backend/src/service/create-feedback.service.ts
+++ backend/src/service/create-feedback.service.ts
@@ -1,7 +1,12 @@
+import { NodemailerMailAdapter } from "../adapters/nodemailer/nodemailer-mail.adapter";
+import { FeedbacksRepository } from "../repositories/models/feedbacks.repository";
 import { CreateFeedbackDto } from "./dto/create-feedback.dto";
 
 export class CreateFeedbackService {
-  constructor() {}
+    constructor(
+        private readonly feedbacksRepository: FeedbacksRepository,
+        private readonly mailAdapter: NodemailerMailAdapter
+      ) {}
 
   async execute(createFeedbackDto: CreateFeedbackDto) {
     const { type, comment, screenshot } = createFeedbackDto;
@@ -17,5 +22,15 @@ export class CreateFeedbackService {
     if (screenshot && !screenshot.startsWith("data:image/png;base64")) {
       throw new Error("Invalid screenshot");
     }
+
+    await this.mailAdapter.sendMail({
+        subject: 'Feedback do usuário',
+        body: [
+          `<div style="font-family: sans-serif; font-size= 16px; c olor=#111">`,
+          `<p>Tipo do feedback: ${type}</p>`,
+          `<p>Comentário: ${comment}</p>`,
+          `</div>`
+        ].join('\n')
+      });
   }
 }
