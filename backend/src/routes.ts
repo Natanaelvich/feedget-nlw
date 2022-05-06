26570f1a35d5557008d4440a6534829e1c0bb80b
Natanael Lima <taelima1997@gmail.com>
2022-05-05 21:19:48 -0300

    update route feedback

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,12 +1,19 @@
 import express from "express";
+import { NodemailerMailAdapter } from "./adapters/nodemailer/nodemailer-mail.adapter";
+import { PrismaFeedbackRepository } from "./repositories/prisma/prisma-feedback-repository";
 import { CreateFeedbackService } from "./service/create-feedback.service";
 
 export const routes = express.Router();
 routes.post("/feedback", async (req, res) => {
   const { type, comment, screenshot } = req.body;
 
-  const createFeedbackService = new CreateFeedbackService();
 
+  const prismaFeedbackRepository = new PrismaFeedbackRepository();
+  const nodemailerMailAdapter = new NodemailerMailAdapter();
+  const createFeedbackService = new CreateFeedbackService(
+    prismaFeedbackRepository,
+    nodemailerMailAdapter
+  );
   await createFeedbackService.execute({
     type,
     comment,
