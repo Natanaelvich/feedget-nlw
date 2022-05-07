18e51b8f46583acf39614cb743a370bdda6c20d5
Natanael Lima <taelima1997@gmail.com>
2022-05-06 23:46:14 -0300

    fix textarea style

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -61,7 +61,7 @@ export function FeedbackContentStep({
 
       <form className="my-4 w-full" onSubmit={handleSubmitFeedback}>
         <textarea
-          className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 bg-transparent border-zinc-600 rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none focus:outline-none scrollbar scrollbar-thumb -zinc-700 scrollbar-track-transparent scrollbar-thin"
+          className="w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 bg-transparent border-zinc-600 rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none focus:outline-none scrollbar scrollbar-thumb -zinc-700 scrollbar-track-transparent scrollbar-thin"
           placeholder="Conte com detalhes o que está acontecendo..."
           onChange={(e) => setComment(e.target.value)}
         ></textarea>
