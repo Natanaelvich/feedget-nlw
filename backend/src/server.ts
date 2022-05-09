c850a08fe449301a7000396ae0c8b243cbb803b9
Natanael Lima <taelima1997@gmail.com>
2022-05-08 22:04:49 -0300

    add app use bodyparser

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import { routes } from './routes';
 import cors from 'cors'
+import bodyParser from 'body-parser';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(routes);
 
 app.listen(3333).on('listening', () => {
