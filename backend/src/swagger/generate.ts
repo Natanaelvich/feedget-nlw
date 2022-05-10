6c485ce06410eac2d99a6944749ccc6d86a0c0f5
Natanael Lima <taelima1997@gmail.com>
2022-05-09 23:59:59 -0300

    update config generator

--- backend/src/swagger/generate.ts
+++ backend/src/swagger/generate.ts
@@ -1,15 +1,33 @@
-const swaggerAutogen = require('swagger-autogen')();
+const swaggerAutogen = require("swagger-autogen")({ openapi: "3.0.0" });
 
 const doc = {
-    info: {
-      title: 'Feedget API',
-      description: 'Get Feedbacks user clients',
+  info: {
+    title: "Feedget API",
+    description: "Get Feedbacks user clients",
+  },
+  host: "localhost:3000",
+  schemes: ["http"],
+  components: {
+    "@schemas": {
+      createFeedback: {
+        type: "object",
+        properties: {
+          type: {
+            type: "string",
+          },
+          comment: {
+            type: "string",
+          },
+          screenshot: {
+            type: "string",
+          },
+        },
+      },
     },
-    host: 'localhost:3000',
-    schemes: ['http'],
-  };
+  },
+};
 
-const outputFile = './src/swagger/swagger_output.json';
-const endpointsFiles = ['./src/server.ts'];
+const outputFile = "./src/swagger/swagger_output.json";
+const endpointsFiles = ["./src/server.ts"];
 
-swaggerAutogen(outputFile, endpointsFiles,doc);
+swaggerAutogen(outputFile, endpointsFiles, doc);
