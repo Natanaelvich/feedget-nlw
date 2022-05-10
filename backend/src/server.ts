7279dae6fd12fe67768c7894eaba9de5c6f63ee8
Natanael Lima <taelima1997@gmail.com>
2022-05-09 23:35:22 -0300

    add use route swagger

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import { routes } from './routes';
 import cors from 'cors'
 import bodyParser from 'body-parser';
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from './swagger/swagger_output.json';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(routes);
 
