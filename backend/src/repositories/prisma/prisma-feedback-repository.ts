13bb0a0f0e67162babc7728ea705d3fa804554b6
Natanael Lima <taelima1997@gmail.com>
2022-05-11 23:42:58 -0300

    update PrismaFeedbackRepository implementation

--- backend/src/repositories/prisma/prisma-feedback-repository.ts
+++ backend/src/repositories/prisma/prisma-feedback-repository.ts
@@ -1,3 +1,4 @@
+import { Feedback } from "@prisma/client";
 import { prisma } from "../../prisma";
 import {
   CreateFeedbackRepositoryDto,
@@ -5,6 +6,10 @@ import {
 } from "../models/feedbacks.repository";
 
 export class PrismaFeedbackRepository implements FeedbacksRepository {
+  findAll() {
+    return prisma.feedback.findMany();
+  }
+
   async create({ comment, type, screenshot }: CreateFeedbackRepositoryDto) {
     await prisma.feedback.create({
       data: {
