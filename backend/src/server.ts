f5f4a2c5f1ed57c8b6372802434bfa6d20217c3e
Natanael Lima <taelima1997@gmail.com>
2022-05-12 23:52:29 -0300

    add rate limit config

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,8 +5,18 @@ import bodyParser from 'body-parser';
 import swaggerUi from 'swagger-ui-express';
 import swaggerFile from './swagger/swagger_output.json';
 
+import rateLimit from 'express-rate-limit'
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 1000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
 const app = express();
 app.use(cors());
+app.use(limiter)
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerFile));
