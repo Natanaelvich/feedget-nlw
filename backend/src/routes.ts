8e45150b0e52c776b2343da6c2bcc5450772ceb5
Natanael Lima <taelima1997@gmail.com>
2022-05-13 23:50:52 -0300

    add validation on route create feedback

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,8 +1,31 @@
 import express from "express";
+import { body, validationResult } from "express-validator";
 import { NodemailerMailAdapter } from "./adapters/nodemailer/nodemailer-mail.adapter";
 import { CreateFeddbackController } from "./controllers/CreateFeddbackController";
 import { GetAllFeddbacksController } from "./controllers/GetAllFeddbacksController";
 
 export const routes = express.Router();
-routes.post("/feedback", new CreateFeddbackController().handle);
+routes.post(
+  "/feedback",
+  body("type")
+    .isString()
+    .withMessage("Type is not string")
+    .notEmpty()
+    .withMessage("Type is required"),
+  body("comment")
+    .isString()
+    .withMessage("Type is not string")
+    .notEmpty()
+    .withMessage("Type is required"),
+  body("screenshot").optional(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    next();
+  },
+  new CreateFeddbackController().handle
+);
 routes.get("/feedback", new GetAllFeddbacksController().handle);
