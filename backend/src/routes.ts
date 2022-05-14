9c4aed02d15a684f5007cd615d8907a1319ea969
Natanael Lima <taelima1997@gmail.com>
2022-05-13 23:41:24 -0300

    update routes with controllers

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,57 +1,8 @@
 import express from "express";
 import { NodemailerMailAdapter } from "./adapters/nodemailer/nodemailer-mail.adapter";
-import { PrismaFeedbackRepository } from "./repositories/prisma/prisma-feedback-repository";
-import { CreateFeedbackService } from "./service/create-feedback.service";
-import { GetAllFeedbackService } from "./service/getall-feedback.service";
+import { CreateFeddbackController } from "./controllers/CreateFeddbackController";
+import { GetAllFeddbacksController } from "./controllers/GetAllFeddbacksController";
 
 export const routes = express.Router();
-routes.post("/feedback", async (req, res) => {
-/*
-#swagger.tags = ['Feedback']
-#swagger.description = 'Route for create feedback.'
-#swagger.requestBody = {
-required: true,
-schema: { $ref: "#/definitions/createFeedback" }
-} */
-
-  try {
-    const { type, comment, screenshot } = req.body;
-
-    const prismaFeedbackRepository = new PrismaFeedbackRepository();
-    const nodemailerMailAdapter = new NodemailerMailAdapter();
-    const createFeedbackService = new CreateFeedbackService(
-      prismaFeedbackRepository,
-      nodemailerMailAdapter
-    );
-
-    await createFeedbackService.execute({
-      type,
-      comment,
-      screenshot,
-    });
-
-    return res.status(201).send();
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
-});
-routes.get("/feedback", async (req, res) => {
-/*
-#swagger.tags = ['Feedback']
-#swagger.description = 'Route for find all feedback.'
-} */
-
-  try {
-    const prismaFeedbackRepository = new PrismaFeedbackRepository();
-
-    const getAllFeedbackService = new GetAllFeedbackService(
-        prismaFeedbackRepository
-    );
-
-    const feedbacks  = await getAllFeedbackService.execute();
-
-    return res.json(feedbacks);
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
-});
+routes.post("/feedback", new CreateFeddbackController().handle);
+routes.get("/feedback", new GetAllFeddbacksController().handle);
