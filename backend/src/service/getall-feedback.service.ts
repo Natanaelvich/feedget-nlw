9f097265004d7589483bdd113977ff36f7e1abf2
Natanael Lima <taelima1997@gmail.com>
2022-05-15 22:58:02 -0300

    add cache on get feedbacks

--- backend/src/service/getall-feedback.service.ts
+++ backend/src/service/getall-feedback.service.ts
@@ -1,11 +1,22 @@
+import redis from "../lib/cache";
 import { FeedbacksRepository } from "../repositories/models/feedbacks.repository";
 
 export class GetAllFeedbackService {
-  constructor(
-    private readonly feedbacksRepository: FeedbacksRepository,
-  ) {}
+  constructor(private readonly feedbacksRepository: FeedbacksRepository) {}
 
   async execute() {
-   return this.feedbacksRepository.findAll()
+    const cacheKey = "feedbacks:all";
+
+    const cachedFeedbacks = await redis.get(cacheKey);
+
+    if (cachedFeedbacks) {
+      return JSON.parse(cachedFeedbacks);
+    }
+
+    const feedbacks = await this.feedbacksRepository.findAll();
+
+    await redis.set(cacheKey, JSON.stringify(feedbacks));
+
+    return feedbacks;
   }
 }
