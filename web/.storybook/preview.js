6cbdfc93ac3fbecd02cdaa88641c99e503d53310
Natanaelvich <taelima1997@gmail.com>
2022-10-08 10:46:56 -0300

    add: storybook config

---
+++ web/.storybook/preview.js
@@ -0,0 +1,20 @@
+import '../src/styles/global.css'
+import { themes } from "@storybook/theming";
+import { initialize, mswDecorator } from 'msw-storybook-addon';
+
+initialize();
+
+export const decorators = [mswDecorator];
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  docs : {
+    theme : themes.dark
+  }
+}
